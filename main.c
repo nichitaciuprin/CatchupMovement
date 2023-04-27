325afff67b1450e54a3a988778067b9b11696aa8
Nichita Ciuprin <nichitaciuprin@gmail.com>
2023-04-27 14:21:20 +0300

    Refactoring

--- main.c
+++ main.c
@@ -5,59 +5,59 @@
 #include "tools/Console.h"
 #include "tools/Window.h"
 
-float _Apos = WindowXLeft;
-float _Avel = 0;
-float _Bpos = WindowXCenter;
-float _Bvel = 0;
-float _Bpos2 = 0;
-int _state = 0;
+float Apos = WindowXLeft;
+float Avel = 0;
+float Bpos = WindowXCenter;
+float Bvel = 0;
+float Bpos2 = 0;
+int state = 0;
 
-float GetSpeed(float position)
+float Bspeed()
 {
     float cap = 3000;
-    float duno = MathMod(position,cap);
+    float duno = MathMod(Bpos,cap);
     float t = MathInverseLerp(0,cap,duno);
     if (t < 0.2f) return 100;
     if (t < 0.5f) return 200;
     return 300;
 }
-void UpdateA(float* Apos, float* Avel, float Bpos, float Bvel, float deltaTime)
+void UpdateA(float deltaTime)
 {
-    MathCatchup(&_Apos,&_Avel,400,_Bpos,_Bvel,deltaTime);
+    MathCatchup(&Apos,&Avel,400,Bpos,Bvel,deltaTime);
 }
-void UpdateB(float* Bpos, float* Bvel, float deltaTime)
+void UpdateB(float deltaTime)
 {
-    switch (_state)
+    switch (state)
     {
         case 0:
         {
-            // _Bpos2 += 100*deltaTime;
-            // *Bpos += GetSpeed(*Bpos)*deltaTime;
-            // *Bpos = WindowXLeft + MathPingPong(WindowWidth2/2+_Bpos2,WindowWidth2);
-            // _Bpos2 = MathPingPong(_Bpos2+deltaTime,WindowWidth2);
+            // Bpos2 += 100*deltaTime;
+            // *Bpos += Bspeed(*Bpos)*deltaTime;
+            // *Bpos = WindowXLeft + MathPingPong(WindowWidth2/2+Bpos2,WindowWidth2);
+            // Bpos2 = MathPingPong(Bpos2+deltaTime,WindowWidth2);
             break;
         }
         case 1:
         {
-            *Bpos = WindowMousePosition.x;
-            *Bvel = WindowMouseVelocity.x;
+            Bpos = WindowMousePosition.x;
+            Bvel = WindowMouseVelocity.x;
             break;
         }
     }
 }
 void Update(float deltaTime)
 {
-    UpdateB(&_Bpos,&_Bvel,deltaTime);
-    UpdateA(&_Apos,&_Avel,_Bpos,_Bvel,deltaTime);
+    UpdateB(deltaTime);
+    UpdateA(deltaTime);
 }
 void HandleInput()
 {
-    if (IsKeyPressed('1')) { _state = 0; return; }
-    if (IsKeyPressed('2')) { _state = 1; return; }
+    if (IsKeyPressed('1')) { state = 0; return; }
+    if (IsKeyPressed('2')) { state = 1; return; }
 }
 int main(void)
 {
-    WindowRender(_Apos,_Bpos);
+    WindowRender(Apos,Bpos);
     HandleInput();
     TimeWaitLoopMark();
 
@@ -67,7 +67,7 @@ int main(void)
 
         Update(TimeFixedDeltaTimeFloat);
 
-        WindowRender(_Apos,_Bpos);
+        WindowRender(Apos,Bpos);
         HandleInput();
         TimeWaitLoopMark();
     }
